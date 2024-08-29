effc5cf6e0dff87b2ea567821055b0a1f7668b73
Upsetti <Alexeth@outlook.com>
2024-08-29 14:14:04 -0500

    initial commit

---
+++ LABS/LAB01/lab.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void isPrime(){
+    int givenNumber;
+    scanf("%d", &givenNumber);
+    if(givenNumber % 2 == 0) printf("The number is Prime");
+    else printf("The number is not Prime");
+    return;
+}
