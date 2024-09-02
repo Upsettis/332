9ec7798672661192079e36445d6c068c4eabc7af
Alex Etheridge <107449728+Upsettis@users.noreply.github.com>
2024-09-01 20:42:36 -0500

    Update lab.c

--- LABS/LAB01/lab.c
+++ LABS/LAB01/lab.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void isPrime(){
-    int givenNumber;
-    scanf("%d", &givenNumber);
-    if(givenNumber % 2 == 0) printf("The number is Prime");
-    else printf("The number is not Prime");
-    return;
+
+//Function isPrime(int n)
+//This function determines if a number n is prime by looping 1 - n and seeing if any number leaves a remained of 0 when n mod i is called
+//returns false if not prime, returns true if prime
+
+bool isPrime(int n){
+    for(int i =1; i < n; i++){
+        if((n % i) == 0) return false;
+    }
+    return true;
 }
 
 int main(){
